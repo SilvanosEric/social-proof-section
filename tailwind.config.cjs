1a37b993516ec40d6652d4452b37ef2ebc6792d6
silvanos <ericoderito@gmail.com>
2022-07-29 16:01:17 +0300

    complete mobile layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,11 +4,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "p-very-dark-magenta": " hsl(300, 43%, 22%)",
-        "p-very-soft-pink": "hsl(333, 80%, 67%)",
-        "n-dark-grayish-magenta": "hsl(303, 10%, 53%)",
-        "n-light-grayish-magenata": "hsl(300, 24%, 96%)",
-        "n-white": "hsl(0, 0%, 100%)",
+        "p-very-dark-magenta": " hsl(300, 43%, 22%)", // card background | heading | ratings
+        "p-very-soft-pink": "hsl(333, 80%, 67%)", // card subtitle
+        "n-dark-grayish-magenta": "hsl(303, 10%, 53%)", // hero copy
+        "n-light-grayish-magenata": "hsl(300, 24%, 96%)", // card copy
+        "n-white": "hsl(0, 0%, 100%)", // body background
       },
       fontSize: {
         body: "15px",
